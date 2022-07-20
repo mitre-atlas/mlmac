6c8fa5fed7312c4295e35bacf61bb7afd2af5192
lilyjw <lilyjw@users.noreply.github.com>
2022-07-20 21:01:56 +0000

    deploy: 43e32d95b92b388935c0548e3fe46a4c4e94d457

--- _nuxt/static/1658341955/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmlmac\u002F_nuxt\u002Fstatic\u002F1658341955",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmlmac\u002F",assetsPath:"\u002Fmlmac\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3220d85"}}}}(null));
