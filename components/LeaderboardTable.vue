8614bb433cada71c3f56dc66848ef28775d0e6cc
Lily Wong <lwong@mitre.org>
2022-07-22 00:29:39 +0000

    Add leaderboard fields and ranking

--- components/LeaderboardTable.vue
+++ components/LeaderboardTable.vue
@@ -5,14 +5,36 @@
     :items="items"
     show-expand
     item-key="name"
+    :search="search"
   >
+    <!--
+    <template #top>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search by name"
+        single-line
+        hide-details
+      ></v-text-field>
+    </template>
+    -->
     <template #item.rank="{ item }">
       {{ item.rank }}
       <span v-if="item.rank === 1">
         <v-icon right small> mdi-crown </v-icon>
       </span>
     </template>
-    <template #item.score="{ item }"> {{ item.score }}% </template>
+    <template #item.lastSubmission="{ item }">
+      {{
+        item.lastSubmission.toLocaleString(undefined, {
+          dateStyle: 'short',
+          timeStyle: 'short'
+        })
+      }}
+    </template>
+    <template #item.score="{ item }">
+      {{ item.numCorrect }} / {{ numModels }}
+    </template>
     <template #expanded-item="{ headers, item }">
       <td :colspan="headers.length">
         <v-sparkline
@@ -44,7 +66,8 @@ export default Vue.extend({
       headers: [
         {
           text: 'Rank',
-          value: 'rank'
+          value: 'rank',
+          filterable: false
         },
         {
           text: 'Name',
@@ -52,13 +75,29 @@ export default Vue.extend({
         },
         {
           text: 'Score',
-          value: 'score'
+          value: 'score',
+          filterable: false
+        },
+        {
+          text: 'Queries',
+          value: 'queries',
+          filterable: false
+        },
+        {
+          text: 'Last submission',
+          value: 'lastSubmission',
+          filterable: false
         },
         {
           text: 'Show queries',
-          value: 'data-table-expand'
+          value: 'data-table-expand',
+          filterable: false
         }
-      ]
+      ],
+      numModels: 12,
+      end: Date.now(),
+      start: new Date().setDate(0),
+      search: ''
     }
   },
   computed: {
@@ -74,7 +113,11 @@ export default Vue.extend({
     items(): Array<object> {
       const teams = this.names.map((name) => ({
         name,
-        score: Math.floor(Math.random() * 100),
+        lastSubmission: new Date(
+          this.start + Math.random() * (this.end - this.start)
+        ),
+        numCorrect: Math.floor(Math.random() * this.numModels),
+        queries: Math.floor(Math.random() * 100),
         rank: 0,
         sparklineValues: Array.from({ length: 10 }, () =>
           Math.floor(Math.random() * 100)
@@ -83,7 +126,25 @@ export default Vue.extend({
       }))
 
       // Reverse score order
-      teams.sort((a, b) => b.score - a.score)
+      teams.sort((a, b) => {
+        // num correct, then queries, then date
+        if (a.numCorrect > b.numCorrect) {
+          // Higher number of correct is better
+          return -1
+        } else if (a.numCorrect === b.numCorrect) {
+          // If the number of correct is the smae
+          if (a.queries < b.queries) {
+            // Lower number of queries is better
+            return -1
+          } else if (a.queries === b.queries) {
+            // If the number of queries is the same, compare the last submission times
+            // Earlier last submission time is better
+            return a.lastSubmission.getTime() - b.lastSubmission.getTime()
+          }
+        }
+        // Otherwise rank b second
+        return 1
+      })
 
       return teams.map((t, i) => {
         t.rank = i + 1
--- components/Overview.vue
+++ components/Overview.vue
@@ -1,21 +1,6 @@
 <template>
   <v-container>
     <SectionHeader id="overview">Overview</SectionHeader>
-    <!-- <p>
-      ML Model (Neural Network) attribution identifies which pre-trained model
-      was used to construct a subsequent model, e.g., via fine-tuning. This
-      could allow intellectual property theft or malicious AI code to be traced
-      back to its origin pre-trained model (e.g., identifying which base model a
-      disinformation bot was derived from).
-    </p>
-    <p>
-      Developing AI Forensics capabilities and establishing the hardness of
-      attribution is important for AI security. This challenge could contribute
-      to the maturation of AI Forensics as a subfield of AI security. If Blue
-      Teams cannot win for multiple rounds, that establishes that the
-      attribution problem is hard much more effectively than directly trying to
-      convince someone.
-    </p> -->
     <nuxt-content :document="doc"></nuxt-content>
   </v-container>
 </template>
