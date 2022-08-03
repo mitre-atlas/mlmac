b40e426cbce2e4d9d028736f6a240cb143b12154
lilyjw <lilyjw@users.noreply.github.com>
2022-08-03 20:49:38 +0000

    deploy: a591298f2c6560d7dc228357c458b51830c53c39

---
+++ _nuxt/static/1659559774/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659559774",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Flogin",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00805784"},http:{browserBaseURL:b}}}}(null,"\u002F"));
---
+++ _nuxt/static/1659559774/submit/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659559774",layout:"default",error:a,state:{isUserAuthenticated:false,userToken:""},serverRendered:true,routePath:"\u002Fsubmit",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00805784"},http:{browserBaseURL:b}}}}(null,"\u002F"));
