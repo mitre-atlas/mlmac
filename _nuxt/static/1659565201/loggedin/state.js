adfb032b00534c4ab6538a7c7978c493f03b4f8c
lilyjw <lilyjw@users.noreply.github.com>
2022-08-03 22:26:09 +0000

    deploy: 773400fd6a5d4587e7f2e4345fc69b57a13d882a

--- _nuxt/static/1659565201/loggedin/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659565201",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Floggedin",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"624a855a"},http:{browserBaseURL:b}}}}(null,"\u002F"));
---
+++ _nuxt/static/1659565563/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flogin","\u002Fsubmit","\u002Fresults","\u002Fterms"]})
