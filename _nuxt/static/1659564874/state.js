78e9fd8c30531ac9ce3ec864689ba480c1596df2
lilyjw <lilyjw@users.noreply.github.com>
2022-08-03 22:20:05 +0000

    deploy: 2be4a8be428942d3fc090e7092bf5953ecb79d4e

--- _nuxt/static/1659564874/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659564874",layout:"default",error:b,state:{isUserAuthenticated:false,userToken:""},serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},content:{dbHash:"58a6dfda"},http:{browserBaseURL:a}}}}("\u002F",null));
