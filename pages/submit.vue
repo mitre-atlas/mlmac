50f6198dcfb17f128e6869d940bcd6e677acdf20
Lily Wong <lwong@mitre.org>
2022-08-05 15:59:19 -0400

    Embed Google Forms

--- pages/submit.vue
+++ pages/submit.vue
@@ -1,9 +1,15 @@
 <template>
-  <SubmissionForm></SubmissionForm>
+  <div>
+    <div id="ff-compose" class="container"></div>
+    <script
+      async
+      defer
+      src="https://formfacade.com/include/112521180686265964585/form/1FAIpQLSferAdg6VAQVVeXVdSJHKT0e2Kp5VB_jDirR2hvP_m_xrkSkw/bootstrap.js?div=ff-compose"></script>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ResultsPage'
+  name: 'SubmitPage'
 }
 </script>
