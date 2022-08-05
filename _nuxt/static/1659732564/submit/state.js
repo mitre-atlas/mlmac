1d9708ffdd98fa5004e1e3d59ed0b44c704b74c7
lilyjw <lilyjw@users.noreply.github.com>
2022-08-05 21:00:26 +0000

    deploy: 3bb63bc7e5264dd53096277ccf94927fd63dcf45

--- _nuxt/static/1659732564/submit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659732564",layout:"default",error:a,state:{isUserAuthenticated:false},serverRendered:true,routePath:"\u002Fsubmit",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9dea57d1"},http:{browserBaseURL:b}}}}(null,"\u002F"));
