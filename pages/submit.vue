eb36c09e95bba8fa184d028e5ab5580072d47276
Lily Wong <lwong@mitre.org>
2022-08-05 22:28:08 -0400

    gradient

--- pages/submit.vue
+++ pages/submit.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
     <SectionHeader>Submission</SectionHeader>
-    <v-sheet class="mx-auto" color="transparent" flat>
-      <!-- <div id="ff-compose" class="container"></div>
-    <script
-      async
-      defer
-      src="https://formfacade.com/include/112521180686265964585/form/1FAIpQLSferAdg6VAQVVeXVdSJHKT0e2Kp5VB_jDirR2hvP_m_xrkSkw/bootstrap.js?div=ff-compose"></script> -->
-      <!-- <iframe
+    <v-sheet class="mx-auto" flat>
+      <iframe
+        id="cont"
         src="https://docs.google.com/forms/d/e/1FAIpQLSferAdg6VAQVVeXVdSJHKT0e2Kp5VB_jDirR2hvP_m_xrkSkw/viewform?embedded=true"
         width="100%"
-        height="1759"
+        height="2000"
         frameborder="0"
         marginheight="0"
-        marginwidth="0"
-        >Loading…</iframe
-      > -->
-      <div id="cont" v-html="html"></div>
+        marginwidth="0">
+        Loading…
+      </iframe>
     </v-sheet>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SubmitPage',
-  async asyncData({ $http }) {
-    const url =
-      'https://docs.google.com/forms/d/1NmHm1WWwvkTTwb5h24D2SxkEJrX35gnXwwjPp_Q5QlY/viewform'
-    const html = await $http.$get(url, { mode: 'no-cors' })
-    console.log(html)
-    return { html }
-  },
-
-  data() {
-    return {
-      url: 'https://forms.gle/7BvpxPaVtK47nUVD8'
-    }
-  }
+  name: 'SubmitPage'
 }
 </script>
 
@@ -49,11 +31,13 @@ iframe {
   max-width: unset;
   width: 1000px;
 }
-#cont >>> .lrKTG .F9yp7e .cBGGJ {
-  /* Entire form, text color */
-}
-#cont >>> .m7w29c {
-  /* Title box */
-  background-color: transparent;
+.v-sheet {
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(248, 249, 250, 1) 80%,
+    rgba(248, 249, 250, 1) 100%
+  );
 }
 </style>
