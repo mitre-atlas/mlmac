3f33d863af2f5faccade26aa54aef3e9d837070c
lilyjw <lilyjw@users.noreply.github.com>
2022-08-08 01:08:06 +0000

    deploy: 7c0643f33ed0448a711e59318e86fca28eae4356

--- _nuxt/static/1659756790/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fapi","\u002F","\u002Flogin","\u002Fresults","\u002Floggedin","\u002Fterms","\u002Fsubmit"]})
---
+++ _nuxt/static/1659920881/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Flogin","\u002Fresults","\u002Fsubmit","\u002Fterms","\u002Fapi","\u002F","\u002Floggedin"]})
