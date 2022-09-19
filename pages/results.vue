628881a326194866380069c0ccc371eb898853c0
Lily Wong <lwong@mitre.org>
2022-09-19 10:28:40 -0400

    Fix last updated time for results

--- pages/results.vue
+++ pages/results.vue
@@ -15,7 +15,7 @@ export default {
     return {
       title: 'Results',
       lastUpdated: `Last updated at ${new Date(
-        '2022-09-17-30T12:00:00Z'
+        '2022-09-17T16:00:00Z'
       ).toLocaleString()}`
     }
   }
